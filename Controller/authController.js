78eb574134de55b96553b5525c271bed657d3d37
GowthamAbi <gowtham2131ece@gmail.com>
2025-09-25 17:00:40 +0530

    logupdate

---
+++ Controller/authController.js
@@ -0,0 +1,6 @@
+const authController=(req,res)=>{
+    res.json({message:"Enter in AuthController"})
+
+}
+
+module.exports=authController
--- Router/router.js
+++ Router/router.js
@@ -1,11 +1,7 @@
 const express=require('express')
+const authController = require('../Controller/authController')
 const router=express.Router()
 
-router.get('/get',(req,res)=>{
-    res.json({message:"hello server"})
-    console.log(req.baseUrl)
-    console.log(req.cookies)
-    console.log(req.method)
-})
+router.get('/get',authController)
 
 module.exports=router
